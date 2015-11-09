6968f912d7a8baa54a95bcf49a2bc6bd058119ff
Jesús Cruz <sezzh@outlook.com>
2015-11-08 19:10:21 -0600

    Usermodule app js complete functionality no comments in the code...

--- tifis_platform/usermodule/urls.py
+++ tifis_platform/usermodule/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^login$', views.view_to_login, name = 'login' ),
     url(r'^getlogin$', views.getlogin, name = 'getlogin'),
     url(r'^logout$', views.log_out, name = 'log_out' ),
+    url(r'^create-new-user$', views.create_new_user, name = 'create_new_user' ),
     url(r'^badlogin/(?P<login_error_message>[0-9]+)$', views.badlogin, name = 'badlogin'),
 ]
--- tifis_platform/usermodule/views.py
+++ tifis_platform/usermodule/views.py
@@ -8,11 +8,31 @@ from django.contrib.auth.models import Group, User
 # Create your views here.
 
 def index(request):
-    """This for check databinding
+    """This for check databinding.
     """
     context = {}
     return render(request, 'usermodule/index.html', context)
 
+def create_new_user(request):
+    """This function is used for create new users throw the login register
+    usermodule.
+    """
+    username = request.POST['username']
+    password = request.POST['password']
+    email = request.POST['email']
+    enrollment = request.POST['enrollment']
+    name = request.POST['name']
+    last_name = request.POST['last_name']
+    mother_last_name = request.POST['mother_last_name']
+    account_type = request.POST['account_type']
+
+    context = {
+        'username': username,
+        'password': password,
+        'enrollment': enrollment,
+        'account_type': account_type,
+    }
+    return render(request, 'usermodule/testview.html', context)
 
 def view_to_login(request):
     """This returns the login for all system.
@@ -45,11 +65,11 @@ def badlogin(request, login_error_message):
     if login_error_message == '1':
         login_error_message = 'El usuario parece estar inactivo...'
     if login_error_message == '2':
-        login_error_message = 'Algo salio mal intenta denuevo...'
+        login_error_message = 'Algo salio mal intenta de nuevo...'
     context = {
         'login_error_message': login_error_message
     }
-    return render(request, 'usermodule/login.html', context)
+    return render(request, 'usermodule/index.html', context)
 
 
 def log_out(request):
